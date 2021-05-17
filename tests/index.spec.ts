67664f3495117d2fd7c001abe7e64c68a90735a3
feng <908087098@qq.com>
2021-05-17 12:01:59 +0800

    init tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,10 @@
+import { strictEqual } from 'assert';
+import { } from '../src';
+
+describe('Typescript usage suite', () =>
+{
+    it('should be able to execute a test', () =>
+    {
+        strictEqual(true, true);
+    });
+});
